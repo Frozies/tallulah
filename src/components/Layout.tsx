290423327e24145fddc01e5d749355be6bfcb4bd
Davin <52686922+Frozies@users.noreply.github.com>
2025-07-25 02:26:46 -0400

    footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import NavBar from './NavBar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-tie-dye bg-cover bg-center animate-pulse-tiedye">
-      <NavBar />
-      <main className="pt-5 max-w-6xl mx-auto px-4">{children}</main>
-    </div>
-  );
-} 
+    return (
+        <div className="min-h-screen flex flex-col bg-tie-dye bg-cover bg-center animate-pulse-tiedye">
+            <NavBar />
+            <main className="pt-5 max-w-6xl mx-auto px-4 flex-grow">{children}</main>
+            <footer className="text-center py-4 text-pink-200 font-medium text-sm">
+                Made with ❤️ for Tallulah
+            </footer>
+        </div>
+    );
+}
